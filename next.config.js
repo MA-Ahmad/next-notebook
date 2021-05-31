bdf09dda204f16a835a7efffdf02c04c7cfc582b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-31 07:58:11 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 // next.config.js
 const withImages = require("next-images");
-module.exports = withImages();
+
+module.exports = withImages({
+  webpack(config, options) {
+    return config;
+  },
+  env: {
+    mongodb_username: "next_blog",
+    mongodb_password: "lbxaIA8rnFhQukEJ",
+    mongodb_clustername: "cluster0.j2ej3.mongodb.net",
+    mongodb_database: "next-blog"
+  }
+});
